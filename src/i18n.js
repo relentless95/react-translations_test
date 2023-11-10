d50f234a7457c3c4ebff4e6a2d8109801fd3d3d9
relentless95 <marcaureltagne@yahoo.com>
2023-11-10 14:12:51 +0100

    implemented simple language switch

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,44 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-// detect user language
-.use(LanguageDetector)
-// pass the i18n instance to react-i18next.
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: {
+          // translations here
+          description: {
+            part1: "Edit <1>src/App.js<1> and save to reload.",
+            part2: "Learn React",
+          },
+          counter_one: "Changed language just once",
+          counter_other: "Changed language already {{count}} times",
+        },
+      },
+
+      de: {
+        translation: {
+          description: {
+            part1: "Andere <1>src/App.js</1> und speichere um neu zu laden.",
+            part2: "Lerne React",
+          },
+
+          counter_one: "Die Sprache wurde erst ein mal gewechselt",
+          counter_other: "Die Sprache wurde {{count}} mal gewechselt",
+        },
+      },
+    },
+  });
+
+export default i18n;
